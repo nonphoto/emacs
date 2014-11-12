b4c7a8d47dd65c1d44e8190454ad8369d40f8fd9
wurflen <jonaskluebbers@gmail.com>
2014-11-12 00:05:42 -0500

    Tweaked term-theme

--- init.el
+++ init.el
@@ -5,19 +5,12 @@
 (setq inhibit-startup-message t)
 (setq initial-scratch-message nil)
 
-;; Display this instead of "For information about GNU Emacs and the
-;; GNU system, type C-h C-a.". This has been made intentionally hard
-;; to customize in GNU Emacs so I have to resort to hackery.
-
 (scroll-bar-mode 0)
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 (fringe-mode 0)
 
-;;(setq-default left-margin-width 2)         
-;;(setq-default right-margin-width 2)
-
-;; Themes
+;; Theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (add-to-list 'load-path "~/.emacs.d/themes")
 (setq-default line-spacing 1)
@@ -27,7 +20,7 @@
 ;; Flyspell often slows down editing so it's turned off
 (remove-hook 'text-mode-hook 'turn-on-flyspell)
 
-;; User files
+;; User files 
 (defconst user-init-dir
   (cond ((boundp 'user-emacs-directory)
 	 user-emacs-directory)
@@ -35,72 +28,11 @@
 	 user-init-directory)
 	(t "~/.emacs.d/")))
 
-
 (defun load-user-file (file)
   (interactive "f")
   "Load a file in current user's configuration directory"
     (load-file (expand-file-name file user-init-dir)))
 
 (load-user-file "modeline.el")
-
-;; Packages
-(require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "http://melpa.org/packages/") t)
-(package-initialize)
-
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
-(require 'multiple-cursors)
-
-(global-set-key (kbd "C-.") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-,") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C->") 'mc/skip-to-next-like-this)
-(global-set-key (kbd "C-<") 'mc/skip-to-previous-like-this)
-
-;; env PATH
-(defun set-exec-path-from-shell-PATH ()
-  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
-    (setenv "PATH" path-from-shell)
-    (setq exec-path (split-string path-from-shell path-separator))))
-
-(defun list-active-modes ()
-  "Give a message of which minor modes are enabled in the current buffer."
-  (interactive)
-  (let ((active-modes))
-    (mapc (lambda (mode) (condition-case nil
-                             (if (and (symbolp mode) (symbol-value mode))
-                                 (add-to-list 'active-modes mode))
-                           (error nil) ))
-          minor-mode-list)
-    (message "Active modes are %s" active-modes)))
-
-(global-set-key (kbd "M-j")
-		(lambda ()
-		  (interactive)
-		  (join-line -1)))
-
-(defun move-line-down ()
-  (interactive)
-  (let ((col (current-column)))
-    (save-excursion
-      (forward-line)
-      (transpose-lines 1))
-    (forward-line)
-    (move-to-column col)))
-
-(defun move-line-up ()
-  (interactive)
-  (let ((col (current-column)))
-    (save-excursion
-      (forward-line)
-      (transpose-lines -1))
-    (forward-line -1)
-    (move-to-column col)))
-
-(define-key input-decode-map "\e\eOA" [(meta up)])
-(define-key input-decode-map "\e\eOB" [(meta down)])
-
-(global-set-key (kbd "M-<down>") 'move-line-down)
-(global-set-key (kbd "M-<up>") 'move-line-up)
+(load-user-file "packages.el")
+(load-user-file "misc.el")
